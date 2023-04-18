9a44d6dd78225cd5bb8460f934ec17344d76c249
Ratiu Cristian <lafurnica2008@yahoo.com>
2023-04-18 21:29:41 +0200

    My noob react project

---
+++ first-project/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import MyName from './MyName';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Helllo nooobs</h1>
+        <h2>Hey mama muie mamamanalllatasdaksfnkafadasdasdaasdasdasdaasdasdadadaf</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ first-project/src/MyName.js
@@ -0,0 +1,8 @@
+
+function MyName() {
+    return (
+        <h1>Testing My Name</h1>
+    )
+}
+
+export default MyName;
